671a3ad88a6bced30cd726c64304556bc59bc44f
Tiago Cordeiro <tiago@mulhergorila.com>
2018-05-13 15:43:30 -0300

    Tentando adaptar para Python 3

--- arauto.py
+++ arauto.py
@@ -3,70 +3,70 @@ import datetime
 import mimetypes
 import os
 
-#TODO migrar para o python3
+# TODO migrar para o python3
 
 version = "1.10"
 
 mimetypes.init()
 
-#mimetypes not supported in default python
-mimetypes.add_type("application/font-woff2",".woff2")
+# mimetypes not supported in default python
+mimetypes.add_type("application/font-woff2", ".woff2")
+
 
 class Arauto():
-    
-    def __init__(self,port=8088):
+
+    def __init__(self, port=8088):
         self.host = ""
-        self.port = port 
+        self.port = port
 
     def run(self):
-        #python's socket magic
+        # python's socket magic
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.s.bind((self.host,self.port))
+        self.s.bind((self.host, self.port))
         self.s.listen(10)
 
-        self.ip = socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))
+        self.ip = socket.gethostbyaddr(
+            socket.gethostbyname(socket.gethostname()))
         self.ip = self.ip[2][0]
 
-
-        #TODO modo silencioso
-        print "*Hello, Master*"
-        print "Serving at: {ip}:{port}".format(ip = self.ip, port = self.port)
-        print
+        # TODO modo silencioso
+        print("*Hello, Master*")
+        print(f"Serving at: {self.ip}:{self.port}")
+        print("")
 
         while True:
-            #TODO multi-threaded server?
+            # TODO multi-threaded server?
             self.conn, self.addr = self.s.accept()
             self.data = self.conn.recv(1024)
 
             try:
-                #get the file name from the URL
+                # get the file name from the URL
                 self.fileRequested = self.getUrl(self.data)
-                
-                #create a response object
+
+                # create a response object
                 self.c = Response(self.addr)
-                
-                #add a file to the response
+
+                # add a file to the response
                 self.c.addFile(self.fileRequested)
 
-                #send the file as a string
+                # send the file as a string
                 self.conn.sendall(str(self.c))
                 self.conn.close()
             except:
                 self.conn.close()
-        
 
-    def getUrl(self,url):
-        #get the file name and path from the get URL
+    def getUrl(self, url):
+        # get the file name and path from the get URL
         url = url.split("\r\n")[0]
         arg = url.split(" ")[1]
         arg = arg.lstrip("/")
-        if(arg == ""):
+        if (arg == ""):
             arg = "index.html"
             return arg
-        if("." not in arg):
-            arg= arg.rstrip("/")
-            arg+="/index.html"
+        if ("." not in arg):
+            arg = arg.rstrip("/")
+            arg += "/index.html"
             return arg
         return arg
 
@@ -79,8 +79,8 @@ class Response():
         and return the object to the browser
 
     '''
-    
-    #default http header
+
+    # default http header
     template = ("{protocol} {status} {nl}"
                 "Location: {location}{nl}"
                 "Date:{date}{nl}"
@@ -90,61 +90,67 @@ class Response():
                 "Connection: {con}{nl}{nl}"
                 "{payload}")
 
-    
-    def __init__(self,addr):
-        self.protocol    = "HTTP/1.1"
-        self.status      = 200
-        self.location    = socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))[0]
-        self.date        = datetime.datetime.now()
-        self.serverName  = "Arauto-"+version
+    def __init__(self, addr):
+        self.protocol = "HTTP/1.1"
+        self.status = 200
+        self.location = \
+        socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))[0]
+        self.date = datetime.datetime.now()
+        self.serverName = "Arauto-" + version
         self.contentType = ""
-        self.contentLen  = ""
-        self.connection  = "close"
-        self.payload     = ""
-        self.addr        = addr
+        self.contentLen = ""
+        self.connection = "close"
+        self.payload = ""
+        self.addr = addr
 
     def __str__(self):
-        return self.template.format(protocol = self.protocol,
-                                    status = self.status,
-                                    location = self.location,
-                                    date = self.date,
-                                    servername = self.serverName,
-                                    contentType = self.contentType,
-                                    contlen = self.contentLen,
-                                    con = self.connection,
-                                    payload = self.payload,
-                                    nl = "\r\n")
+        return self.template.format(protocol=self.protocol,
+                                    status=self.status,
+                                    location=self.location,
+                                    date=self.date,
+                                    servername=self.serverName,
+                                    contentType=self.contentType,
+                                    contlen=self.contentLen,
+                                    con=self.connection,
+                                    payload=self.payload,
+                                    nl="\r\n")
 
-    def addFile(self,file):
+    def addFile(self, file):
         ''' add a file to the response object '''
-        
+
         try:
-            if(file == os.path.basename(__file__)):
+            if (file == os.path.basename(__file__)):
                 raise
-            temp = open(file,"rb").read()
+            temp = open(file, "rb").read()
             filename, file_extension = os.path.splitext(file)
-	    try:
+            try:
                 self.contentType = mimetypes.types_map[file_extension]
-	    except:
-	        print "Files '{0}' not supported yet".format(file_extension)
-            self.contentLen  = len(temp)
-            self.payload = temp
-            stat = "served (200)"
+            except:
+                print(f"Files '{file_extension}' not supported yet")
+                self.contentLen = len(temp)
+                self.payload = temp
+                stat = "served (200)"
         except:
             self.status = 404
             self.contentType = ""
-            self.contentLen  = ""
-            self.payload     = "<h1>File {} not found</h1>".format(file)
+            self.contentLen = ""
+            self.payload = "<h1>File {} not found</h1>".format(file)
             stat = "not found (404)"
         finally:
-            print "{time} - File: '{file}' requested by: {addr} {status}".format(time = datetime.datetime.now(),file=file,status=stat,addr=self.addr[0])
-            
+            time = datetime.datetime.now()
+            file = file
+            status = stat
+            addr = self.addr[0]
+            print(f"{time} - File: '{file}' requested by: {addr} {status}")
+
+
 if __name__ == "__main__":
     import sys
+
     try:
         newPort = int(sys.argv[1])
-        s = Arauto(port = newPort)
+        s = Arauto(port=newPort)
     except:
-        print "[You need admin permissions to run this port, using 8088]"
-    	s = Arauto()
+        print("[You need admin permissions to run this port, using 8088]")
+        s = Arauto()
     s.run()
